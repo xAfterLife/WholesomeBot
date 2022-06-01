35fb36990ca860f5ed1ebd85732db792a0ba6b63
Lucas <lisselhard@aol.de>
2022-06-01 14:21:52 +0200

    Initial Commit

---
+++ WholesomeBot/Modules/TestingModule.cs
@@ -0,0 +1,30 @@
+﻿using Discord.Commands;
+using WholesomeBot.Services;
+
+namespace WholesomeBot.Modules;
+
+// Modules must be public and inherit from an IModuleBase
+public class TestingModule : ModuleBase<SocketCommandContext>
+{
+    // Dependency Injection will fill this value 
+    public VRChatApiService VRChatApiService { get; set; } = null!;
+
+    [Command("debugerror")]
+    public Task DebugError([Remainder] string text)
+    {
+        throw new Exception(text);
+    }
+
+    [Command("joinvrinstance")]
+    public async Task JoinVrInstance([Remainder] string text)
+    {
+        await VRChatApiService.RequestInvite(text);
+    }
+
+    [Command("invitevruser")]
+    public Task InviteVrUser([Remainder] string text)
+    {
+        _ = VRChatApiService.Invite(text);
+        return Task.CompletedTask;
+    }
+}
